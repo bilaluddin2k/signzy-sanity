d435ae9a7f5c0f9236239bc2a0fac344f4971838
bilaluddin2k <98716029+bilaluddin2k@users.noreply.github.com>
2024-03-22 11:47:46 +0530

    Updated Signzy_sanity.sh
    Added more options.
    now the script will print more info.
    Some Bug fix.
    
    ! need to add further more.

--- signzy_sanity.sh
+++ signzy_sanity.sh
@@ -2,18 +2,27 @@
 ################################################################ version 2 ###########################################################################
 
 #!/usr/bin/bash
+# Author: Bilaluddin 
+# Profile: DEVOPS
+# version: V0.3
+
+version=V0.3
 
 echo "this script can only be run on PR and DR"
 
 RED="\e[31m"
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
+BLUE="\e[34m"
+
+echo -e "${BLUE}Version: $version ${ENDCOLOR}"
 
 options(){
     echo -e "${GREEN}----------- Select Nodes Size --------------${ENDCOLOR}"
     echo "1. 3 Node cluster (Web1, Web2, Web4)"
     echo "2. 5 Node cluster (Web1, Web2, web3, Web4, web5)"
     echo "3. Run Basic Sanity"
+    echo "4. check kafka if working/printing"
     echo -e "${GREEN}--------------------------------------------${ENDCOLOR}"
 }
 
@@ -23,12 +32,10 @@ basic_sanity(){
 
 
 
-kafka_msg_printing(){
-    /home2/kafka/bin/kafka-console-consumer.sh --bootstrap-server SP3SIGNZYWEB1PROD:5002,SP3SIGNZYWEB2PROD:5002,SIGNZYWEB4PROD:5002 --topic $topic --max-messages 4
-}
 
-if [ echo $env == "signzyDC" ]; then
-    echo "Current Environment is ${GREEN}$env${ENDCOLOR}"
+
+if [ echo $setup == "DC" ]; then
+    echo "Current Environment is ${GREEN} $setup ${ENDCOLOR}"
     
     3_node_kafka(){
     for x in SP3SIGNZYWEB1PROD SP3SIGNZYWEB2PROD SP3SIGNZYWEB4PROD ; do echo $x; echo srvr|nc $x 2181; echo; done 
@@ -40,6 +47,11 @@ if [ echo $env == "signzyDC" ]; then
     
     
     while true; do
+    
+kafka_msg_printing(){
+    /home2/kafka/bin/kafka-console-consumer.sh --bootstrap-server SP3SIGNZYWEB1PROD:5002,SP3SIGNZYWEB2PROD:5002,SIGNZYWEB4PROD:5002 --topic $topic --max-messages 4
+}
+
     options
     read -p "select any option: " option
     case $option in
@@ -56,7 +68,7 @@ if [ echo $env == "signzyDC" ]; then
 
 else
     
-    echo "Current Environment is ${GREEN}$env${ENDCOLOR}"
+    echo -e "Current Environment is ${GREEN} $setup ${ENDCOLOR}"
     3_node_kafka(){
     for x in DRSP3SIGNZYWEB1PROD DRSP3SIGNZYWEB2PROD DRSP3SIGNZYWEB4PROD ; do echo $x; echo srvr|nc $x 2181; echo; done 
 }
@@ -65,6 +77,11 @@ else
     for x in DRSP3SIGNZYWEB1PROD DRSP3SIGNZYWEB2PROD DRSP3SIGNZYWEB3PROD DRSP3SIGNZYWEB4PROD DRSP3SIGNZYWEB5PROD; do echo $x; echo srvr|nc $x 2181; echo; done
 }
     while true; do
+    
+    kafka_msg_printing(){
+    /home2/kafka/bin/kafka-console-consumer.sh --bootstrap-server DRSP3SIGNZYWEB1PROD:5002,DRSP3SIGNZYWEB2PROD:5002,DRSP3SIGNZYWEB3PROD:5002,DRSP3SIGNZYWEB4PROD:5002,DRSP3SIGNZYWEB5PROD:5002 --topic $topic --max-messages 4
+}
+    
     options
     read -p "select any option: " option
     case $option in
@@ -86,6 +103,7 @@ fi   #first block of if condition ends
 
 
 <<comm
+
 docker ps --format "Container ID: {{.ID}}, Status: {{.Status}}, Names: {{.Names}}"
 /home2/kafka/bin/kafka-console-consumer.sh --bootstrap-server SP3SIGNZYWEB1PROD:5002,SP3SIGNZYWEB2PROD:5002,SIGNZYWEB4PROD:5002 --topic $topic --max-messages 4
 
@@ -100,8 +118,3 @@ if [ "$env" == "signzyDR" ]; then
 fi
 
 comm
-   
- 
-    
-
-    
